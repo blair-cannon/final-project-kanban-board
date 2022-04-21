40837276a9d170ba130771c64d46214ea54f14ef
Blair Preston <blair.preston@student.awesomeinc.org>
2022-04-21 15:54:05 +0000

    create all model classes that do not have foreign keys

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+# Referecned model classes:
+
+class Location(models.Model):
+    city = models.CharField(max_length=150)
+    state = models.CharField(max_length=150)
+    zipcode = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.city
+    
+class Park(models.Model):
+    name = models.CharField(max_length=500)
+    city = models.CharField(max_length=150)
+    state = models.CharField(max_length=150)
+    zipcode = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.name
+
+class Breed(models.Model):
+    name = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+class Gender(models.Model):
+    label = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.label
+
+class Socialization(models.Model):
+    label = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.label
+
+class Aggression(models.Model):
+    label = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.label
+
+class Tag(models.Model):
+    label = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.label
